cc41c8b93ec5c8e46b5cf4d035251fcce40564e3
Faires12 <faressolibie@gmail.com>
2023-01-15 11:57:26 -0300

    ajuste validation

--- src/presentation/validations/validation.ts
+++ src/presentation/validations/validation.ts
@@ -3,11 +3,11 @@ import { ValidationBuilder } from "./"
 
 export class Validation {
     public builder : ValidationBuilder = new ValidationBuilder(new ValidatorAdapter())
-    builderConfig: () => (Error | null)[]
+    builderConfig: () => (Error | null)[] 
 
     validate(input: any) : Error | null {
         this.builder.setInput(input)
-        const errors = this.builderConfig()
+        const errors = this.builderConfig ? this.builderConfig() : []
         for(const error of errors){
             if(error) return error
         }
